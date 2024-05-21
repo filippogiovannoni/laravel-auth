305c851e51d8379724e3a74d2ef8189ba9cc2b6c
filippogiovannoni <filippogiovannonii@gmail.com>
2024-05-21 12:03:07 +0200

    add CRUD routes for admin dashboard

---
+++ resources/views/layouts/admin/projects/index.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.admin')
+
+@section('content')
+    @foreach ($projects as $project)
+    @endforeach
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\ProjectController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -27,6 +28,9 @@ Route::middleware(['auth', 'verified'])
     ->group(function () {
 
         Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+
+        // CRUD
+        Route::resource('projects', ProjectController::class);
     });
 
 
